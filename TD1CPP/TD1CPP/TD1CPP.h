9e3cd3bbd47dfe6de03b15407453d68c8cf789b6
YonaSZL <yona.sazela@gmail.com>
2024-12-06 12:01:27 +0100

    tttttttt

--- TD1CPP/TD1CPP/TD1CPP.h
+++
@@ -1,8 +0,0 @@
-﻿// TD1CPP.h : fichier Include pour les fichiers Include système standard,
-// ou les fichiers Include spécifiques aux projets.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Référencez ici les en-têtes supplémentaires nécessaires à votre programme.
